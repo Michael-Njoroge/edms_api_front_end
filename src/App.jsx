0a32be77e659cbcaa4f483d381013c55df6cd4bf
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-17 12:29:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+ <BrowserRouter>
+ <Header/>
+ <Routes>
+  <Route path='/' element={<Home/>}></Route>
+  <Route path='/sign-in' element={<SignIn/>}></Route>
+ </Routes>
+ </BrowserRouter>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+  return (
+    
+    <header style={{backgroundColor:'rgb(226 232 240)', boxShadow:'0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'}}>
+
+        <div style={{display:'flex', justifyContent:'space-between', alignItems:'center',maxWidth:'72rem',marginLeft:'auto',marginRight:'auto', padding:'0.75rem'}}>
+
+<Link to={'/'} style={{textDecoration:'none'}}>
+        <h1 style={{fontWeight:'bold', fontSize: '14px',flexWrap:'wrap'}}>
+            
+        <span style={{color: 'rgb(100 116 139)'}}>DISI</span>
+        <span style={{color:'rgb(51 65 85)'}}>EDMS</span>
+        </h1>
+        </Link>
+
+        <ul style={{display:'flex', gap:'16px',listStyle:'none',color:'rgb(51 65 85)'}}>
+            <Link to={'/'} style={{textDecoration:'none'}}> <li >Home</li></Link>
+           <Link to={'/sign-in'} style={{textDecoration:'none'}}> <li style={{textDecoration:'none'}}>Sign In</li> </Link>
+        </ul>
+        </div>
+    </header>
+   
+  )
+}
